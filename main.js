994a6d95cb81b164f02695da1a9c852635719f3c
ClownDay <88943993+ClownDay@users.noreply.github.com>
2021-11-23 10:06:31 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+var gameData = {
+  coins: 0
+};
 
+class Generator {
+  constructor(tier, type) {
+    this.tier = tier;
+    this.type = type;
+    this.amount = 0;
+    this.purchased = 0;
+    this.multiplier = 1;
+  }
+  
+  productionPerTick() {
+    return this.amount * this.multiplier;
+  }
+  
+}
+
+const Gen1 = new Generator(1, normal);
